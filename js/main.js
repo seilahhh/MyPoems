2e24eabcfea5efb64c5ba8dfdf713fe169865804
seilahhh <pereirajoaquim312@gmail.com>
2025-12-12 02:05:37 +0000

    First commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#book").turn({
+        width: 1000,
+        height: 600,
+        autoCenter: true,
+        duration: 1000,
+        gradients: true,
+        elevation: 50,
+    });
+});
