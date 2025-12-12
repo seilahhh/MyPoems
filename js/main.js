4a41cef3e070a6f0dbf7185c8a1082f95ed3b86d
seilahhh <pereirajoaquim312@gmail.com>
2025-12-12 03:40:53 +0000

    Improve book css

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ $(document).ready(function () {
     $("#book").turn({
         width: 1000,
         height: 600,
-        autoCenter: true,
-        duration: 1000,
-        gradients: true,
-        elevation: 50,
+        autoCenter: true
     });
 });
